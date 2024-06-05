7e6a96692b11def18f665ccde88e8f40f3f492b4
YY <52943717+18227370901@users.noreply.github.com>
2024-06-05 15:46:59 +0800

    Create base.py

---
+++ Resource/base.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup # type: ignore
+from selenium import webdriver # type: ignore
+from selenium.webdriver.common.by import By # type: ignore
+from selenium.webdriver.common.keys import Keys # type: ignore
+from PIL import Image # type: ignore
+from aip import AipOcr # type: ignore
+import time
+import os
+from selenium.webdriver.chrome.service import Service # type: ignore
+from selenium.webdriver.chrome.options import Options # type: ignore
+import random
